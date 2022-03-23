5a29b601962606d246af4227ef70d7261a8b1923
Alokk <64439434+Wrres@users.noreply.github.com>
2022-03-23 00:27:11 +0000

    Render push

--- src/utils/Helpers.js
+++ src/utils/Helpers.js
@@ -46,6 +46,7 @@ const HELPERS = {
 			\`!rucities #\` - Russian cities 50k+ pop. \`!rutowns\`
 			\`!krcities #\` - South and North Korean cities
 			\`!grplaces #\` - Greek place names (not only Greece)
+			Non-language commands (GeoGuessr/geography):
 			\`!uscapitals #\` - US state capitals \`!usflags\`
 			\`!bollards #\` - guess the country the bollard is from
 			\`!cameras #\` - guess the camera gens in the country
